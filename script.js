d52193e6da302e52539e602594bccd91e5d28675
Karina <karyna.selivanova@nure.ua>
2019-02-04 14:42:39 +0200

    added element`s area, changed start of moments

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 'use strict';
 const randomInteger = (min, max) => {
     const result = Math.round(min - 0.5 + Math.random() * (max - min + 1));
-    return result === 0 ? randomInteger : result;
+    return result === 0 ? randomInteger(min, max) : result;
 };
 
 const selectColor = () => {
@@ -15,17 +15,23 @@ const selectColor = () => {
 
 const drawingWindow = document.getElementById('figures');
 const context = drawingWindow.getContext('2d');
-const arrCircles = new Array(10).fill('').map(() => new Circle());
-const arrSquare = new Array(10).fill('').map(() => new Square());
+const arrCircles = [];
+const arrSquare = [];
+
+
+const fillArr = (arr, obj) => {
+    if (arr.length < 10) arr.push(obj);
+};
 
 function Circle() {
-    this.centerX = drawingWindow.width / 2;
-    this.centerY = drawingWindow.height / 2;
     this.radius = randomInteger(5, 40);
+    this.centerX = 0 + this.radius;
+    this.centerY = 0 + this.radius;
     this.color = selectColor();
     this.dx = randomInteger(-4, 4);
     this.dy = randomInteger(-2, 2);
-}
+    this.area = Math.PI * Math.pow(this.radius, 2);
+};
 
 const drawCircle = (circle) => {
     context.beginPath();
@@ -34,6 +40,7 @@ const drawCircle = (circle) => {
     context.fill();
     context.closePath();
 };
+
 const animationCircle = (circle) => {
     drawCircle(circle);
     if (circle.centerX + circle.dx > drawingWindow.width - circle.radius || circle.centerX + circle.dx < circle.radius) {
@@ -48,11 +55,12 @@ const animationCircle = (circle) => {
 
 function Square() {
     this.side = randomInteger(5, 40);
-    this.x = drawingWindow.width / 2 - this.side / 2;
-    this.y = drawingWindow.height / 2 - this.side / 2;
+    this.x = 0;
+    this.y = 0;
     this.color = selectColor();
     this.dx = randomInteger(-4, 4);
     this.dy = randomInteger(-2, 2);
+    this.area = Math.pow(this.side, 2);
 }
 
 const drawSquare = (square) => {
@@ -63,7 +71,7 @@ const drawSquare = (square) => {
 };
 const animationSquare = (square) => {
     drawSquare(square);
-    if (square.x + square.dx > drawingWindow.width -square.side || square.x + square.dx < 0) {
+    if (square.x + square.dx > drawingWindow.width - square.side || square.x + square.dx < 0) {
         square.dx = -square.dx;
     }
     if (square.y + square.dy > drawingWindow.height - square.side || square.y + square.dy < 0) {
@@ -80,5 +88,9 @@ const startDraw = () => {
 };
 
 window.onload = () => {
+    setInterval(() => fillArr(arrCircles, new Circle()), 5000);
+    setTimeout(() => setInterval(() => fillArr(arrSquare, new Square()), 5000), 55000);
     setInterval(() => startDraw(), 10);
+    setTimeout(() => console.log(arrCircles.map((item) => item.area)), 50000); //show array of circle`s areas
+    setTimeout(() => console.log(arrSquare.map((item) => item.area)), 110000); //show array of square`s areas
 };
